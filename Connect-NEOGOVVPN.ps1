313bcb24803e2d1654636fd5c7ae382f3c1ac5c1
Richard Hopkins <rhopkins@neogov.net>
2021-03-01 14:39:47 -0800

    adding new stuff working with Google Sheets

---
+++ Connect-NEOGOVVPN.ps1
@@ -0,0 +1,36 @@
+﻿<#
+To setup the script do the following...
+
+$username = $env:USERNAME
+$domain = $env:USERDNSDOMAIN
+$AESKey = New-Object Byte[] 32
+[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($AESKey)
+$AESKey | Out-File "C:\$username@$domain.AES.key"
+#You will need to manually type in your password to Read-Host
+$PasswordSecureString = Read-Host -AsSecureString
+$PasswordSecureString | ConvertFrom-SecureString -key $AESKey | Out-File -FilePath "C:\$username@$domain.securestring.txt"
+
+#>
+
+$vpnName = "NEOGOV VPN"
+$username = $env:USERNAME
+$domain = $env:USERDNSDOMAIN
+$AESKey = Get-Content "C:\credFiles\$username@$domain.AES.key"
+$EncryptedPasswordFile = "C:\credFiles\$username@$domain.securestring.txt"
+#Use the Key to decrypt the password and load it into memory as a SecureString
+[SecureString]$SecureStringPassword = Get-Content -Path $EncryptedPasswordFile | ConvertTo-SecureString -Key $AESKey
+#Decrypt the SecureString to String
+[String]$password = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecureStringPassword))
+for ($i = 1; $i -le 3; $i++)
+{
+	$vpn = Get-VpnConnection -Name "NEOGOV VPN"
+	If ($vpn.ConnectionStatus -eq "Disconnected")
+	{
+		rasdial $vpnName $username $password
+	}
+	else
+	{
+		exit 0
+	}
+	Start-Sleep -Seconds 15
+}
---
+++ Invoke-WeblogicJVMUpgrade.ps1
@@ -0,0 +1,47 @@
+﻿function Get-ShortName
+{
+	param ($path)
+	$fso = New-Object -ComObject Scripting.FileSystemObject
+	If ($path.PSIsContainer)
+	{
+		$fso.GetFolder($path.FullName).ShortName
+	}
+	else
+	{
+		$fso.GetFile($path.FullName).ShortName
+	}
+}
+
+#Upgrade java from the installation (Robocopy new version into place, or do you use the Java installer?)
+robocopy "\\PATH\TO\NEW\JDK\jdk1.8.0_281" "D:\Java\JDK\jdk1.8.0_281" /e /mt
+#Copy old cacerrs file into the new JDK
+Copy-Item 'D:\Java\JDK\jdk.1.8.0_151\jre\lib\security\cacerts' 'D:\Java\JDK\jdk1.8.0_261\jre\lib\security\cacerts' -Force
+#Search for all the occurance of older java version and replace it with the new version inside C:\Oracle\Middleware\Oracle_Home
+$files = Get-ChildItem "C:\Oracle\Middleware\Oracle_Home" -File
+foreach ($file in $files)
+{
+	if ((Get-Content $file) -match 'jdk.1.8.0_151')
+	{
+		#backup config before setting the new path
+		Copy-Item -Path $file.FullName -Destination "$($file.FullName).bak" -Force
+		(Get-Content $file).replace('jdk.1.8.0_151', 'jdk1.8.0_261') | Set-Content $file
+	}
+}
+#Search for all the occurance of older java version and replace it with the new version inside C:\webdata (search for short name like JDK18~1.0_2)
+$oldShortName = Get-ShortName -path (Get-Item 'D:\Java\JDK\jdk.1.8.0_151')
+$newShortName = Get-ShortName -path (Get-Item 'D:\Java\JDK\jdk.1.8.0_281')
+$files = Get-ChildItem "C:\webdata" -File
+foreach ($file in $files)
+{
+	if ((Get-Content $file) -match $oldShortName)
+	{
+		#backup config before setting the new path
+		Copy-Item -Path $file.FullName -Destination "$($file.FullName).bak" -Force
+		(Get-Content $file).replace($oldShortName, $newShortName) | Set-Content $file
+	}
+}
+#Go to C:\webdata\domains\Neogov\bin, run the setdomainenv.cmd script after installing the new java.
+$status = Start-Process -FilePath "C:\webdata\domains\Neogov\bin\setdomainenv.cmd"
+#If nodemanager fail to start, uninstall it. Run the uninstallnodemager to uninstall the nodemanager
+#Check $status for error and execute uninstall if found
+#Run installnodemanager to install the nodemanager
